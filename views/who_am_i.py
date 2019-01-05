590646a3a4c840ecba49050ad5dc8a3d9705b5f3
Vivek Chand <V.rajput40@gmail.com>
2019-01-05 20:22:47 +0530

    Add api view to get user information and edit phone visibility status

--- views/who_am_i.py
+++ views/who_am_i.py
@@ -1,3 +1,5 @@
+from itertools import chain
+
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -8,6 +10,8 @@ from kernel.serializers.person import ProfileSerializer
 from kernel.serializers.roles.student import StudentSerializer
 from kernel.serializers.roles.faculty_member import FacultyMemberSerializer
 from kernel.serializers.generics.contact_information import ContactInformationSerializer
+
+from buy_and_sell.models import SaleProduct, RequestProduct
 
 
 
@@ -44,9 +48,48 @@ class WhoAmI(GenericAPIView):
             user = FacultyMemberSerializer(
                 person.facultymember
             ).data
-
+        
         contact_information = ContactInformationSerializer(
             contact_information
         ).data
+        is_phone_visible = False
+        sale_product = SaleProduct.objects.filter(person= person)
+        request_product = RequestProduct.objects.filter(person= person)
+        all_results = list(chain(sale_product, request_product))
+        for product in all_results:
+            if(product.is_phone_visible == True):
+                is_phone_visible=True
+                break
+        user['is_phone_visible'] = is_phone_visible
         user['person']['contact_information'] = contact_information
         return Response(user, status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        """
+        View to serve POST requests
+        :param request: the request that is to be responded to
+        :param args: arguments
+        :param kwargs: keyword arguments
+        :return: the response for request
+        """
+
+        person = request.person
+        try:
+            phone_status = request.data['phone_status']
+            sale_product = SaleProduct.objects.filter(person= person)
+            request_product = RequestProduct.objects.filter(person= person)
+            all_results = list(chain(sale_product, request_product))
+            for product in all_results :
+                if(phone_status == "true"):
+                    product.is_phone_visible = True
+                    product.save()
+                elif(phone_status == "false"):
+                    product.is_phone_visible = False
+                    product.save()
+            return Response(data={
+                        'phone_status': 'phone status successfully changed.'
+                        }, status=status.HTTP_200_OK)
+        except:
+            return Response(data={
+                        'error': 'phone status was not changed due to some error.'
+                        }, status=status.HTTP_400_BAD_REQUEST)
